e623e894fdd7b2e92718ff4dfe760766bd1e46fe
Bayu Maulana <bayufransdo@gmail.com>
2023-10-18 16:55:40 +0700

    whatgpt3 section

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from 'react';
-import tetew from './header.css';
+import './header.css';
 import people from '../../assets/people.png';
 import ai from '../../assets/ai.png';
 
